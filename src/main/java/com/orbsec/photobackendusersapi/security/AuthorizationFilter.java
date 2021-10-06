9e52b193d1278dd6d7c5b630c80b682785f163ce
Petre Vane <petre.vane@gmail.com>
2021-10-06 12:08:29 +0200

    Added AuthorizationFilter.java. This will ensure  that the generated Auth Token is valid

---
+++ src/main/java/com/orbsec/photobackendusersapi/security/AuthorizationFilter.java
@@ -0,0 +1,66 @@
+package com.orbsec.photobackendusersapi.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+    private final Environment environment;
+
+    public AuthorizationFilter(AuthenticationManager authManager, Environment environment) {
+        super(authManager);
+        this.environment = environment;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        String authorizationHeader = req.getHeader("Authorization");
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer")) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(req, res);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest req) {
+        String authorizationHeader = req.getHeader("Authorization");
+
+        if (authorizationHeader == null) {
+            return null;
+        }
+
+        final String emptyString  = "";
+        String token = authorizationHeader.replace("Bearer", emptyString);
+
+        String userId = Jwts.parser()
+                .setSigningKey(environment.getProperty("token.secret"))
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        if (userId == null) {
+            return null;
+        }
+
+        return new UsernamePasswordAuthenticationToken(userId, null, new ArrayList<>());
+
+    }
+}
